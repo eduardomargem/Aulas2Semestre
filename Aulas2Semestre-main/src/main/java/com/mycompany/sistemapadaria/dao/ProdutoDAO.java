ad63b81001d00fa105d79270f8f7a98a9458706a
Eduardo Margem <eduardojesus6733@gmail.com>
2024-05-20 21:26:52 -0300

    FUNCAO CADASTRAR COMPLETA ProdutoDAO

--- Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/dao/ProdutoDAO.java
+++ Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/dao/ProdutoDAO.java
@@ -17,30 +17,35 @@ import java.util.logging.Logger;
  * @author eduardo.mcjesus
  */
 public class ProdutoDAO {
+
     public static String url = "jdbc:mysql://localhost:3306/sistemaPadaria";
     public static String usuario = "root";
     public static String senha = "";
-    
-    public static boolean cadastrar(Produto obj){
+
+    public static boolean cadastrar(Produto obj) {
         boolean retorno = false;
         Connection conexao = null;
 
         try {
             // 1) Carregar o Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            
             // 2) Abrir conexão
             conexao = DriverManager.getConnection(url, usuario, senha);
-            
+
             // 3) Preparar o comando SQL
-            PreparedStatement comandoSQL = conexao.prepareStatement("INSERT INTO produto (nome, cpf, telefone, email, sexo, civil, nascimento, endereco, numeroCasa, bairro, cidade, complemento, cep, estado) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            
+            PreparedStatement comandoSQL = conexao.prepareStatement("INSERT INTO produto (idProduto, nomeProduto, descricaoProduto, precoProduto, categoriaProduto, validadeProduto, volumeProduto, observacaoProduto) values (?, ?, ?, ?, ?, ?, ?, ?)");
+            comandoSQL.setInt(1,obj.getIdProduto());
+            comandoSQL.setString(2,obj.getNomeProduto());
+            comandoSQL.setString(3,obj.getDescricaoProduto());
+            comandoSQL.setFloat(4,obj.getPrecoProduto());
+            comandoSQL.setString(5,obj.getCategoriaProduto());
+            comandoSQL.setDate(6,new java.sql.Date( obj.getValidadeProduto().getTime()));
+            comandoSQL.setString(7,obj.getVolumeProduto());
+            comandoSQL.setString(8,obj.getObservacaoProduto());
             // 4) Passar parâmetros para o comando SQL
-            
-            
             // 5) Executar o comando
             int linhasAfetadas = comandoSQL.executeUpdate();
-            
+
             if (linhasAfetadas > 0) {
                 // Gravou com sucesso!
                 retorno = true;
@@ -50,8 +55,9 @@ public class ProdutoDAO {
         } catch (SQLException ex) {
             Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return retorno;
-    
+
     }
+    
 }
--- Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/models/Produto.java
+++ Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/models/Produto.java
@@ -3,63 +3,26 @@ package com.mycompany.sistemapadaria.models;
 import java.util.Date;
 
 public class Produto {
-    
+    private int idProduto;
     private String nomeProduto;
-<<<<<<< HEAD
     private String descricaoProduto;
     private float precoProduto;
     private String categoriaProduto;
     private Date validadeProduto;
     private String volumeProduto;
-    private String observacoesProduto;
-
-    public Produto(){
-        
-    }
-    public String getNomeProduto() {
-        return nomeProduto;
-    }
-=======
-    private String descricao;
-    private float preco;
-    private String categoria;
-    private Date validade;
-    private String volume;
-    private String Observacoes;
+    private String observacaoProduto;
 
     public String getNomeProduto() {
         return nomeProduto;
     }
-    
-    public String getDescricao() {
-        return descricao;
-    }
-    
-     public float getPreco() {
-        return preco;
-    }
-      
-    public String getCategoria() {
-        return categoria;
-    }
-     public Date getValidade() {
-        return validade;
-    }
-     
-    public String getVolume() {
-        return volume;
-    }
-    
-    public String getObservacoes() {
-        return Observacoes;
+
+    public int getIdProduto() {
+        return idProduto;
     }
 
->>>>>>> be64f95c8aaf1b601481fcaf8fb9a04bc80145bc
-
-    public void setNomeProduto(String nomeProduto) {
-        this.nomeProduto = nomeProduto;
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
     }
-<<<<<<< HEAD
 
     public String getDescricaoProduto() {
         return descricaoProduto;
@@ -101,40 +64,13 @@ public class Produto {
         this.volumeProduto = volumeProduto;
     }
 
-    public String getObservacoesProduto() {
-        return observacoesProduto;
+    public String getObservacaoProduto() {
+        return observacaoProduto;
     }
 
-    public void setObservacoesProduto(String observacoesProduto) {
-        this.observacoesProduto = observacoesProduto;
+    public void setObservacaoProduto(String observacaoProduto) {
+        this.observacaoProduto = observacaoProduto;
     }
 
-   
-=======
- 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-     public void setPreco(float preco) {
-        this.preco = preco;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public void setValidade(Date validade) {
-        this.validade = validade;
-    }
-
-    public void setVolume(String volume) {
-        this.volume = volume;
-    }
-
-    public void setObservacoes(String Observacoes) {
-        this.Observacoes = Observacoes;
-    }
->>>>>>> be64f95c8aaf1b601481fcaf8fb9a04bc80145bc
-      
+  
 }
