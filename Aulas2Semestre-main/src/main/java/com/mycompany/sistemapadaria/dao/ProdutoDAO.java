067c1ea0b70682eb76de135be54e2626a9db707a
Eduardo Margem <eduardojesus6733@gmail.com>
2024-05-26 20:12:42 -0300

    JAVADOC ADICIONADO - ProdutoDAO

--- Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/dao/ProdutoDAO.java
+++ Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/dao/ProdutoDAO.java
@@ -19,12 +19,23 @@ import java.util.logging.Logger;
  *
  * @author eduardo.mcjesus
  */
+
+/** 
+ * Classe para conexão do sistema ao banco de dados
+ * @version 1.0.0
+ * @since 2024-05-13
+ */
 public class ProdutoDAO {
 
     public static String url = "jdbc:mysql://localhost:3306/sistemapadaria";
     public static String usuario = "root";
     public static String senha = "admin";
-
+    /**
+     * Abre a conexão com o banco de dados e cadastra um produto.
+     * 
+     * @param obj O objeto Produto a ser cadastrado.
+     * @return true se o produto foi cadastrado com sucesso, false caso contrário.
+     */
     public static boolean cadastrar(Produto obj) {
         boolean retorno = false;
         Connection conexao = null;
@@ -37,6 +48,7 @@ public class ProdutoDAO {
 
             // 3) Preparar o comando SQL
             PreparedStatement comandoSQL = conexao.prepareStatement("INSERT INTO produto (idProduto, nomeProduto, descricaoProduto, precoProduto, categoriaProduto, validadeProduto, volumeProduto, quantidadeProduto, observacaoProduto) values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            // 4) Passar parâmetros para o comando SQL
             comandoSQL.setInt(1, obj.getIdProduto());
             comandoSQL.setString(2, obj.getNomeProduto());
             comandoSQL.setString(3, obj.getDescricaoProduto());
@@ -46,7 +58,6 @@ public class ProdutoDAO {
             comandoSQL.setString(7, obj.getVolumeProduto());
             comandoSQL.setInt(8, obj.getQuantidadeProduto());
             comandoSQL.setString(9, obj.getObservacaoProduto());
-            // 4) Passar parâmetros para o comando SQL
             // 5) Executar o comando
             int linhasAfetadas = comandoSQL.executeUpdate();
 
@@ -62,7 +73,12 @@ public class ProdutoDAO {
 
         return retorno;
     }
-
+    /**
+     * Abre a conexão com o banco de dados e altera um produto existente.
+     * 
+     * @param obj O objeto Produto a ser alterado.
+     * @return true se o produto foi alterado com sucesso, false caso contrário.
+     */
     public static boolean alterar(Produto obj) {
         boolean retorno = false;
         Connection conexao = null;
@@ -99,7 +115,12 @@ public class ProdutoDAO {
 
         return retorno;
     }
-
+    /**
+     * Abre a conexão com o banco de dados e exclui um produto pelo seu ID.
+     * 
+     * @param idExcluir O ID do produto a ser excluído.
+     * @return true se o produto foi excluído com sucesso, false caso contrário.
+     */
     public static boolean excluir(int idExcluir) {
         boolean retorno = false;
         Connection conexao = null;
@@ -127,7 +148,11 @@ public class ProdutoDAO {
 
         return retorno;
     }
-
+    /**
+     * Abre a conexão com o banco de dados e lista todos os produtos.
+     * 
+     * @return Uma lista de objetos Produto.
+     */
     public static ArrayList<Produto> listar() {
         ArrayList<Produto> lstProduto = new ArrayList<>();
         Connection conexao = null;
@@ -163,7 +188,12 @@ public class ProdutoDAO {
         }
         return lstProduto;
     }
-
+     /**
+     * Abre a conexão com o banco de dados e busca produtos por categoria.
+     * 
+     * @param categoriaProduto A categoria dos produtos a serem buscados.
+     * @return Uma lista de objetos Produto que pertencem à categoria especificada.
+     */
     public static ArrayList<Produto> buscarCategoria(String categoriaProduto) {
         ArrayList<Produto> lstProduto = new ArrayList<>();
         Connection conexao = null;
@@ -200,7 +230,12 @@ public class ProdutoDAO {
         }
         return lstProduto;
     }
-    
+    /**
+     * Abre a conexão com o banco de dados e busca produtos por data de validade.
+     * 
+     * @param validadeProduto A data de validade dos produtos a serem buscados.
+     * @return Uma lista de objetos Produto que possuem a data de validade especificada.
+     */
     public static ArrayList<Produto> buscarValidade(Date validadeProduto){
         ArrayList<Produto> lstProduto = new ArrayList<>();
         Connection conexao = null;
