0d4ba9fe7a2af2cf17b4147c9d12eb40e29ffc1f
Eduardo Margem <eduardojesus6733@gmail.com>
2024-05-27 16:48:06 -0300

    CLASSE VENDA ATUALIZADA

--- Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/dao/VendaDAO.java
+++ Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/dao/VendaDAO.java
@@ -4,10 +4,102 @@
  */
 package com.mycompany.sistemapadaria.dao;
 
+import static com.mycompany.sistemapadaria.dao.ProdutoDAO.senha;
+import static com.mycompany.sistemapadaria.dao.ProdutoDAO.url;
+import static com.mycompany.sistemapadaria.dao.ProdutoDAO.usuario;
+import com.mycompany.sistemapadaria.models.Produto;
+import com.mycompany.sistemapadaria.models.Venda;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author eduardo.mcjesus
  */
 public class VendaDAO {
-    
+    public static String url = "jdbc:mysql://localhost:3306/sistemapadaria";
+    public static String usuario = "root";
+    public static String senha = "admin";
+    public static List<Produto> listar() {
+        List<Produto> lstProduto = new ArrayList<>();
+        Connection conexao = null;
+        ResultSet rs = null;
+
+        try {        
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            PreparedStatement comandoSQL = conexao.prepareStatement("SELECT * FROM produto");
+            rs = comandoSQL.executeQuery();
+            
+            while (rs.next()) {
+                int id = rs.getInt("idProduto");
+                String nome = rs.getString("nomeProduto");
+                String descricao = rs.getString("descricaoProduto");
+                float preco = rs.getFloat("precoProduto");
+                String categoria = rs.getString("categoriaProduto");
+                Date dataValidade = new java.util.Date(rs.getDate("validadeProduto").getTime());
+                String volume = rs.getString("volumeProduto");
+                int quantidade = rs.getInt("quantidadeProduto");
+                String observacoes = rs.getString("observacaoProduto");
+
+                Produto item = new Produto(id, nome, descricao, preco, categoria, dataValidade, volume, quantidade, observacoes);
+                lstProduto.add(item);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (conexao != null) conexao.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return lstProduto;
+    }
+    public static boolean cadastrar(Venda obj) {
+        boolean retorno = false;
+        Connection conexao = null;
+
+        try {
+            // 1) Carregar o Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // 2) Abrir conexão
+            conexao = DriverManager.getConnection(url, usuario, senha);
+
+            // 3) Preparar o comando SQL
+            PreparedStatement comandoSQL = conexao.prepareStatement("INSERT INTO venda (idVenda, itemVenda, cpf, nomeProduto, quantidade, formaPagamento");
+            // 4) Passar parâmetros para o comando SQL
+            comandoSQL.setInt(1, obj.getIdVenda());
+            comandoSQL.setInt(2, obj.getItemVenda());
+            comandoSQL.setString(3, obj.getCliente().getCpf());
+            comandoSQL.setString(4, obj.getProduto().getNomeProduto());
+            comandoSQL.setInt(5, obj.getQuantidade());
+            comandoSQL.setString(6, obj.getFormaPagamento());
+
+            // 5) Executar o comando
+            int linhasAfetadas = comandoSQL.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                // Gravou com sucesso!
+                retorno = true;
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return retorno;
+    }
 }
--- Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/models/Venda.java
+++ Aulas2Semestre-main/src/main/java/com/mycompany/sistemapadaria/models/Venda.java
@@ -1,48 +1,102 @@
 
 package com.mycompany.sistemapadaria.models;
 
+import java.util.List;
+
 /**
  *
  * @author Vinicius Damaceno
  */
 public class Venda {
-    
-public class CarrinhoDeCompras {
-    private String produto;
+    private int idVenda;
+    private int itemVenda;
+    private Cliente cliente;
+    private Produto produto;
     private int quantidade;
-    private double preco;
-    private String categoria;
+    private float preco;
+    private String formaPagamento;
 
-        public String getProduto() {
-            return produto;
-        }
-
-        public void setProduto(String produto) {
-            this.produto = produto;
-        }
-
-        public int getQuantidade() {
-            return quantidade;
-        }
-
-        public void setQuantidade(int quantidade) {
-            this.quantidade = quantidade;
-        }
-
-        public double getPreco() {
-            return preco;
-        }
-
-        public void setPreco(double preco) {
-            this.preco = preco;
-        }
-
-        public String getCategoria() {
-            return categoria;
-        }
-
-        public void setCategoria(String categoria) {
-            this.categoria = categoria;
-        }
+    public Venda(int cliente, String produto, int quantidade, float preco, String categoria, String formaPagamento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
+    public Venda(String cliente, String produto, int quantidade, float preco, String categoria, String formaPagamento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Venda(String cliente, String produto, String quantidade, float preco, String categoria, String formaPagamento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Venda(int cliente, String produto, int quantidade, float preco, String categoria) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public Venda(int itemVenda, Cliente cliente, Produto produto, int quantidade) {
+        this.itemVenda = itemVenda;
+        this.cliente = cliente;
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Venda(String cliente, List<String> produtosSelecionados, String quantidade, String preco, String categoria) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public int getIdVenda() {
+        return idVenda;
+    }
+
+    public void setIdVenda(int idVenda) {
+        this.idVenda = idVenda;
+    }
+
+    public int getItemVenda() {
+        return itemVenda;
+    }
+
+    public void setItemVenda(int itemVenda) {
+        this.itemVenda = itemVenda;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
 }
